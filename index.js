4d7c257bdc5a4f1f6ed6e578c872424b91a1d295
pawan-kumar-dev <pawan.kv612@gmail.com>
2021-07-13 20:06:33 +0530

    updates

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
 $(document).ready(function () {
   $(".center").slick({
-    centerMode: true,
+    dots: true,
+    infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    dots: true,
     arrows: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 1,
-        },
-      },
-    ],
   });
 
   function createHomePageProductCard(obj) {
